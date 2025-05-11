f6868a9069b9ec0f2611724afd632ded58ad012c
Artem Vazhentsev <artiomvazh99@gmail.com>
2025-05-11 13:48:30 +0300

    fix

--- src/lm_polygraph/stat_calculators/attention_fp.py
+++ src/lm_polygraph/stat_calculators/attention_fp.py
@@ -49,10 +49,18 @@ class AttentionForwardPassCalculator(StatCalculator):
 
         for i in range(len(texts)):
             input_ids = torch.cat([batch['input_ids'], torch.tensor([cut_sequences[i]]).to(model.device())], axis=1)
-            with torch.no_grad():
-                forwardpass_attentions = model.model(input_ids, output_attentions=True).attentions
-                forwardpass_attentions = tuple(attention.to("cpu") for attention in forwardpass_attentions)
-                forwardpass_attentions = torch.cat(forwardpass_attentions).float().numpy()
+            try:
+                with torch.no_grad():
+                    forwardpass_attentions = model.model(input_ids, output_attentions=True).attentions
+                    forwardpass_attentions = tuple(attention.to("cpu") for attention in forwardpass_attentions)
+                    forwardpass_attentions = torch.cat(forwardpass_attentions).float().numpy()
+            except:
+                with torch.no_grad():
+                    forwardpass_attentions = model.model(input_ids[-1024:], output_attentions=True).attentions
+                    forwardpass_attentions = tuple(attention.to("cpu") for attention in forwardpass_attentions)
+                    forwardpass_attentions = torch.cat(forwardpass_attentions).float().numpy()
+                
+            torch.cuda.empty_cache()
             forwardpass_attention_weights.append(forwardpass_attentions)
             
         forwardpass_attention_weights = np.array(forwardpass_attention_weights)
