73d8b746d355265fd8fcbf8b49f3e9a287c7789c
Artem Vazhentsev <artiomvazh99@gmail.com>
2025-11-01 20:37:19 +0300

    fix

--- src/lm_polygraph/utils/manager.py
+++ src/lm_polygraph/utils/manager.py
@@ -697,8 +697,9 @@ class UEManager:
                     for stat, stat_value in new_stats.items():
                         if stat in batch_stats.keys():
                             continue
-                        if not len(stat_value):
-                            continue
+                        if not isinstance(stat_value, int):
+                            if not len(stat_value):
+                                continue
                         batch_stats[stat] = stat_value
                 for stat in batch_stats.keys():
                     if "embeddings_all" in stat:
